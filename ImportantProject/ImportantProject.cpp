42c3cecc41905438f1b7e4530f9ee1cd90b98c9f
Vladimir Khusainov <vladimir_husainov@mail.ru>
2024-02-13 18:04:08 +0300

    убрал ненужный код branch addPoster

--- ImportantProject/ImportantProject.cpp
+++ ImportantProject/ImportantProject.cpp
@@ -12,14 +12,7 @@ int main()
 	for (int i = 0; i < 10; i++)
 	{
 		j += i;
-	}
-	for (int i = 0; i < 10; i++)
-	{
-		j += i;
-	}
-	for (int i = 0; i < 10; i++)
-	{
-		j += i;
+	
 	}
 	//ConnectionDb con = new Connection(DB_LOGIN, DB_PAWSSORD);
 }
