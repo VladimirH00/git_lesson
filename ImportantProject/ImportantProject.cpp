64450f4396f34a0a022942e84ae6f12e8409aade
Vladimir Khusainov <vladimir_husainov@mail.ru>
2024-02-13 17:35:25 +0300

    Наш первый комит инициализации

---
+++ ImportantProject/ImportantProject.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!\n";
+
+	cout << "important info project";
+
+	//ConnectionDb con = new Connection(DB_LOGIN, DB_PAWSSORD);
+}
